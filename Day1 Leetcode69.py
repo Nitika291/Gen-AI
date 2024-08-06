758088f4f346576edcfae9ed66ad01142cdbdb28
Nitika291 <nitika2022sangwan@gmail.com>
2024-08-06 20:58:05 +0530

    Create Day1 Leetcode69.py

---
+++ Day1 Leetcode69.py
@@ -0,0 +1,26 @@
+class Solution:
+    def mySqrt(self, x):
+        if x == 0:
+            return 0
+        
+        left, right = 1, x
+        result = 0
+        
+        while left <= right:
+            mid = (left + right) // 2
+            mid_squared = mid * mid
+            
+            if mid_squared == x:
+                return mid
+            elif mid_squared < x:
+                result = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return result
+
+"""
+Input: x = 4
+Output: 2
+"""
